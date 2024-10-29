17d457032114412a7dc50e7e9a813ec316d28e53
Nicholas <uvanicholasj@gmail.com>
2024-10-29 21:19:29 +0000

    training logging

--- train_eval.py
+++ train_eval.py
@@ -19,7 +19,7 @@ from morph_model import MorphM2M100, MorphModelDataCollator
 
 import evaluate
 import logging
-
+from datetime import datetime
 
 #import function_trace
 #some stuff that will be used everywhere perhaps
@@ -72,6 +72,10 @@ def compute_metrics(
     #load_metric is deprecated, need to use evaluate.load() from the library hf evaluate
     #bleu_metric = load_metric("sacrebleu", trust_remote_code=True)
     #accuracy_metric = load_metric("accuracy", trust_remote_code=True)
+
+    #for i in range(len(decoded_labels)):
+        #print(f"pred {i}: {decoded_preds[i]}")
+        #print(f"label {i}: {decoded_labels[i]}")
 
     bleu_metric = evaluate.load('sacrebleu')
     #accuracy_metric = evaluate.load('accuracy')
@@ -158,7 +162,7 @@ def tokenize_dataset(
         print(f"split: {split}")
         dataset = dataset_dict[split]
         inputs = [ex for ex in dataset[f"{src_lang}_text"]]
-        targets = [ex for ex in dataset[f"{src_lang}_text"]]
+        targets = [ex for ex in dataset[f"{trg_lang}_text"]]
         tags_data = [ex for ex in dataset[f'{src_lang} tags']]
         tags_data = [' '.join(tag for tag in tags) for tags in tags_data]
         tags_data = [sent.replace('+', '') for sent in tags_data]
@@ -172,21 +176,27 @@ def tokenize_dataset(
     return tokenized_dataset_dict
 
 #in use
-def gen_training_args():
+def gen_training_args(
+        log_filepath
+):
     training_args = Seq2SeqTrainingArguments(
-        output_dir='./test_results',
+        #output_dir='./tt-en-init-test_results',
+        output_dir=f"./{log_filepath}/training_results/",
+        logging_dir=f"./{log_filepath}/training_logs/",
         fp16=True,
-        eval_strategy='steps',
+        eval_strategy='epoch', #'no', 'steps', or 'epoch'
         per_device_train_batch_size=1,
         per_device_eval_batch_size=1,
         #learning_rate=5e-5,
         weight_decay=0.01,
-        logging_dir='./test_logs',
-        logging_steps=100,
+        #logging_dir='./test_logs',
+        logging_steps=500,
         save_steps=500,
-        num_train_epochs=1,
+        num_train_epochs=3,
         predict_with_generate=True,
-        load_best_model_at_end=False,
+        load_best_model_at_end=True,
+        metric_for_best_model='eval_bleu',
+        greater_is_better=True,
         save_total_limit=3,
         warmup_steps=10,
         #label_smoothing_factor=0.1
@@ -202,11 +212,13 @@ def morph_train_with_trainer(
         trg_lang,
         to_train=False,
         to_eval=False,
+        save_model=False,
+        save_to_cumulative=False,
         proportion_of_train_dataset=1.0,
-        proportion_of_test_dataset=1.0
+        proportion_of_test_dataset=1.0,
+        mid_training_eval_sent_num=100
         ):
-    #src_lang = 'fi'
-    #trg_lang = 'en'
+    cumulative_model_path_holder_filename = "./cumulative_model.txt"
     trg_lang_nllb_code = utils.get_nllb_code(trg_lang)
     trg_lang_nllb_id = text_tokenizer.convert_tokens_to_ids(trg_lang_nllb_code)
     #text_tokenizer = NllbTokenizer.from_pretrained(initial_checkpoint, src_lang=utils.get_nllb_code(src_lang))
@@ -215,7 +227,7 @@ def morph_train_with_trainer(
         data_collator = MorphModelDataCollator(text_tokenizer, model)
     else:
         data_collator = DataCollatorForSeq2Seq(text_tokenizer, model)
-    training_args = gen_training_args()
+    
     #model.gradient_checkpointing_enable()
     optimizer = AdamW(
     #optimizer = Adafactor(
@@ -230,32 +242,58 @@ def morph_train_with_trainer(
         train_dataset = data['train']
     else:
         train_dataset = data['train'].train_test_split(test_size=proportion_of_train_dataset)['test']
+    mid_training_eval_proportion = mid_training_eval_sent_num / len(data['test'])
+    if mid_training_eval_proportion > 1.0:
+        mid_training_eval_proportion = 1.0
+    mid_training_eval_set = data['test'].train_test_split(test_size=mid_training_eval_proportion)['test']
     if proportion_of_test_dataset>=1.0:
         test_dataset = data['test']
     else:
         test_dataset = data['test'].train_test_split(test_size=proportion_of_test_dataset)['test']
 
-
-    #trainer = MorphSeq2SeqTrainer(
-    trainer = Seq2SeqTrainer(
-    #trainer = ForwardOnlyTrainer(
-        model=model,
-        args=training_args,
-        train_dataset=train_dataset,
-        eval_dataset=test_dataset,
-        data_collator=data_collator,
-        compute_metrics=compute_metrics,
-        optimizers=(optimizer, None),
-    )
+    #TRAINING
     if to_train:
-        trainer.train()
+        training_args = gen_training_args()
+        training_trainer = Seq2SeqTrainer(
+            model=model,
+            args=training_args,
+            train_dataset=train_dataset,
+            eval_dataset=mid_training_eval_set,
+            data_collator=data_collator,
+            compute_metrics=compute_metrics,
+            optimizers=(optimizer, None),
+        )
+        #does train return anything? I just added the output
+        results = training_trainer.train()
+        best_model_dir = training_trainer.state.best_model_checkpoint
+        
+
+    #EVALUATION
     if to_eval:
-        results = trainer.evaluate(forced_bos_token_id=trg_lang_nllb_id)
+        eval_args = gen_training_args()
+        eval_trainer = Seq2SeqTrainer(
+            model=model,
+            args=eval_args,
+            #train_dataset=train_dataset,
+            eval_dataset=test_dataset,
+            data_collator=data_collator,
+            compute_metrics=compute_metrics,
+            optimizers=(optimizer, None),
+        )
+        results = eval_trainer.evaluate(forced_bos_token_id=trg_lang_nllb_id)
         #results = trainer.evaluate()
         print(f"results: {results}")
-        with open('fi-en-morph-embed-dim-cat-first_try_model_results.txt', 'a') as sink:
-            sink.write(str(results))
-    trainer.save_model('./fi-en-morph-embed-dim-cat-first_try_test_model')
+        #with open('fi-en-morph-embed-dim-cat-first_try_model_results.txt', 'a') as sink:
+            #sink.write(str(results))
+        best_model_dir = eval_trainer.state.best_model_checkpoint
+
+
+    if save_model:
+        if save_to_cumulative:
+            with open(cumulative_model_path_holder_filename, 'w') as sink:
+                sink.write(best_model_dir)
+
+    #trainer.save_model('./fi-en-morph-embed-dim-cat-first_try_test_model')
     
     return
 
@@ -263,10 +301,18 @@ def training_pipeline(
         initial_checkpoint,
         src_lang,
         trg_lang,
+        load_from_disk=False,
+        model_disk_filepath=None,
+        save_model=False,
         run_baseline=False,
         to_train=True,
-        to_eval=True
+        to_eval=True,
+        proportion_of_train_dataset=1.0,
+        proportion_of_test_dataset=1.0
 ):
+    
+    all_logs_filepath = f"{src_lang}-{trg_lang}_train-{str(to_train)}_eval-{to_eval}_{datetime.now().strftime("%Y-%m-%d_%H-%M")}"
+    train_results_filepath = f"./{all_logs_filepath}/train_results"
     src_nllb = utils.get_nllb_code(src_lang)
     trg_nllb = utils.get_nllb_code(trg_lang)
     text_tokenizer = NllbTokenizer.from_pretrained(
@@ -286,12 +332,29 @@ def training_pipeline(
     else:
         model = create_model()
 
-    morph_train_with_trainer(model, dataset_dict, text_tokenizer, to_train=to_train, to_eval=to_eval)
+
+
+    morph_train_with_trainer(
+        model,
+        dataset_dict,
+        text_tokenizer,
+        src_lang=src_lang,
+        trg_lang=trg_lang,
+        to_train=to_train,
+        to_eval=to_eval,
+        proportion_of_train_dataset=proportion_of_train_dataset,
+        proportion_of_test_dataset=proportion_of_test_dataset
+    )
     
 
     return
 
 def main() -> None:
+
+
+
+
+
     #model = T5ForConditionalGeneration.from_pretrained(model_checkpoint)
     #tokenizer = T5Tokenizer.from_pretrained(model_checkpoint)
     #vocab = tokenizer.get_vocab()
@@ -310,6 +373,21 @@ def main() -> None:
     print(f"cuda is available: {cuda.is_available()}")
     print(f"cuda current device: {cuda.current_device()}")
     print(f"cuda current device name: {cuda.get_device_name(cuda.current_device())}")
+
+    #oct 28 test
+    initial_checkpoint = 'facebook/nllb-200-distilled-600M'
+    training_pipeline(
+        initial_checkpoint=initial_checkpoint,
+        src_lang='tt',
+        trg_lang='en',
+        run_baseline=False,
+        to_train=True,
+        to_eval=True,
+        proportion_of_train_dataset=0.1,
+        proportion_of_test_dataset=0.1
+    )
+    return
+
     #cuda.empty_cache()
     #cuda.reset_max_memory_allocated()
     #print(cuda.memory_summary())
